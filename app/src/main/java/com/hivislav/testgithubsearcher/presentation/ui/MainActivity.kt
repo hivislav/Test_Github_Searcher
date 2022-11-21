001957f76c19d41116920d8f414b6334135bbfe9
hivislav <hivislav@gmail.com>
2022-11-21 16:55:20 +0600

    task 8.22 - create theme
    // added splash screen

--- app/src/main/java/com/hivislav/testgithubsearcher/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/hivislav/testgithubsearcher/presentation/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import com.hivislav.testgithubsearcher.R
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_TestGithubSearcher)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
